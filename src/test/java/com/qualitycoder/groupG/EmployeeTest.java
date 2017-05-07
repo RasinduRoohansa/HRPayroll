23851546a5ba6ab930a4487fda330d69896f6f73
Amila Kumara <amilarajans@gmail.com>
2017-05-07 14:33:05 +0530

    modified employee

--- src/test/java/com/qualitycoder/groupG/EmployeeTest.java
+++ src/test/java/com/qualitycoder/groupG/EmployeeTest.java
@@ -1,28 +1,21 @@
 package com.qualitycoder.groupG;
 
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 /**
  * Created by pravindap on 5/7/17.
  */
 public class EmployeeTest {
-    @Test
-    public void testAddEmployee(){
+	@Test
+	public void testAddEmployee() {
 
-    }
-    @Test
-    public void test_View_Employee_Details(){
+	}
 
-        Employee emp= new Employee("E00002","JOHN COOPER",'M',
-                "12/10/1983","SERGEANT",85600.0,10.0,
-                "0783557787","49,Wijayaba Mawatha,Dehiwela");
-        ArrayList<Employee> employees=new ArrayList<Employee>();
-        employees.add(emp);
+	@Test
+	public void test_View_Employee_Details() {
+		Employee emp = new Employee("E00002", "JOHN COOPER", 'M',
+				"12/10/1983", "SERGEANT", 85600.0, 10.0,
+				"0783557787", "49,Wijayaba Mawatha,Dehiwela");
 
-        Employee empActual=emp.viewEmployeeDetails("E00002",employees);
-        Assert.assertEquals(empActual, emp);
-    }
+	}
 }

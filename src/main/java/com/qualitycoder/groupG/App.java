d827d40077859ad2c47311cd6883f77e7bed43bd
Amila Kumara <amilarajans@gmail.com>
2017-05-07 15:37:04 +0530

    added log4j2 logging configuration

--- src/main/java/com/qualitycoder/groupG/App.java
+++ src/main/java/com/qualitycoder/groupG/App.java
@@ -1,7 +1,15 @@
 package com.qualitycoder.groupG;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by pravindap on 5/7/17.
  */
 public class App {
+	private static final Logger LOGGER= LogManager.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("hi");
+	}
 }

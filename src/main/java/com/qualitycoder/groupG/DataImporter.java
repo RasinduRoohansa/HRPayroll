18e3b90255a5fa059ef6e93e708f6fff557fc501
RasinduRoohansa <rasindu@sp-solutions.biz>
2017-05-07 15:01:27 +0530

    data importer fixes added

--- src/main/java/com/qualitycoder/groupG/DataImporter.java
+++ src/main/java/com/qualitycoder/groupG/DataImporter.java
@@ -20,11 +20,11 @@ public class DataImporter {
                 employee.setName(!data[1].equals("Name")?data[1]:null);
                 employee.setDesignation(!data[2].equals("Job Titles")?data[2]:null);
                 employee.setDob(!data[3].equals("Date of Birth")?data[3]:null);
-                employee.setDob(!data[4].equals("Gender")?data[4]:null);
-                employee.setDob(!data[5].equals("Basic Salary (LKR)")?data[5]:null);
-                employee.setDob(!data[6].equals("EPF Contribution")?data[6]:null);
-                employee.setDob(!data[7].equals("Phone Number")?data[7]:null);
-                employee.setDob(!data[8].equals("Address")?data[8]:null);
+                employee.setGender(!data[4].equals("Gender")?data[4].charAt(0):null);
+                employee.setBasicSalary(!data[5].equals("Basic Salary (LKR)")?Double.parseDouble(data[5]):null);
+                employee.setEpfContribution(!data[6].equals("EPF Contribution")?Double.parseDouble(data[6]):null);
+                employee.setPhoneNumber(!data[7].equals("Phone Number")?data[7]:null);
+                employee.setAddress(!data[8].equals("Address")?data[8]:null);
                 employeeList.add(employee);
             }
         }catch (Exception exception){

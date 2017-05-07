889ee2f402f925777dd920930f840927d51a7c1c
kasunqc17 <kasunqc17@gmail.com>
2017-05-07 14:19:09 +0530

    benifit super class travel allowance calculation overtime calculation medical fee calculation medical fee calculation test case when employee works one year medical fee calculation test case when employee works two years medical fee calculation test case when employee works three years medical fee calculation test case when employee works four years medical fee calculation test case when employee works five years employee four hours regular overtime test case
    travel allowence calculation when employee has a car
    travel allowance calculation when employee do not have a car

---
+++ src/test/java/com/qualitycoder/groupG/MedicalFeeTest.java
@@ -0,0 +1,52 @@
+package com.qualitycoder.groupG;
+
+import org.junit.Test;
+
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by iSeeQ on 5/7/2017.
+ */
+public class MedicalFeeTest {
+    @Test
+    public void calculateMedicalFee_when_EmployeeHasOneYearExpiriance(){
+
+        MedicalFee medicalFee=new MedicalFee(1);
+        double employeeMedicalAllowance=medicalFee.calculate();
+        assertThat(employeeMedicalAllowance,is(50000.0));
+    }
+
+    @Test
+    public void calculateMedicalFee_when_EmployeeHasTwoYearExpiriance(){
+
+        MedicalFee medicalFee=new MedicalFee(2);
+        double employeeMedicalAllowance=medicalFee.calculate();
+        assertThat(employeeMedicalAllowance,is(50000.0));
+    }
+
+    @Test
+    public void calculateMedicalFee_when_EmployeeHasThreeYearsExpiriance(){
+
+        MedicalFee medicalFee=new MedicalFee(3);
+        double employeeMedicalAllowance=medicalFee.calculate();
+        assertThat(employeeMedicalAllowance,is(100000.0));
+    }
+
+    @Test
+    public void calculateMedicalFee_when_EmployeeHasFourYearsExpiriance(){
+
+        MedicalFee medicalFee=new MedicalFee(4);
+        double employeeMedicalAllowance=medicalFee.calculate();
+        assertThat(employeeMedicalAllowance,is(100000.0));
+    }
+
+    @Test
+    public void calculateMedicalFee_when_EmployeeHasFiveYearsExpiriance(){
+
+        MedicalFee medicalFee=new MedicalFee(5);
+        double employeeMedicalAllowance=medicalFee.calculate();
+        assertThat(employeeMedicalAllowance,is(500000.0));
+    }
+}
---
+++ src/test/java/com/qualitycoder/groupG/OvertimeTest.java
@@ -0,0 +1,23 @@
+package com.qualitycoder.groupG;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by iSeeQ on 5/7/2017.
+ */
+public class OvertimeTest {
+    @Test
+    public void calculateOverTime_when_EmployeeFourHoursRegularOverTime(){
+        int workedHours=4;
+        String overTimePackage="Regular";
+        double hourlyRate=1000.00;
+        Overtime overtime=new Overtime(workedHours,overTimePackage,hourlyRate);
+        double employeeOvertime=overtime.calculate();
+        assertThat(employeeOvertime,is(6000));
+    }
+
+}
+
